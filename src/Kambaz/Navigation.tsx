6c43561296415fbdc4f302b4c7cfb2f7711e2b4b
Aryan Mehta <aryanmehta@Aryans-MacBook-Air.local>
2025-02-05 23:10:39 -0500

    minor changes2

--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -18,7 +18,7 @@ export default function KambazNavigation() {
         target="_blank"
         className="list-group-item bg-black border-0 text-center"
       >
-        <img src="public/images/NEU.png" width="75px" />
+        <img src="public/images/neu.png" width="75px" />
 
       </a>
       <Link
